f1e7598a07fb559889bbc531aee31f974d9f4ad8
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2023-10-25 08:46:05 -0400

    nested rutes

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule,
+  imports: [MatButtonModule,MatToolbarModule,MatIconModule,
     CommonModule
   ],
-  exports: [MatButtonModule]
+  exports: [MatButtonModule,MatToolbarModule,MatIconModule]
 })
 export class MaterialModule { }
